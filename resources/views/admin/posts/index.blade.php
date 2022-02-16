a6042573146ed11b803d6c2b6a36894d1688bd4a
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2022-02-16 21:51:47 +0100

    Creata tabella nella index

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,21 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">Lista posts</div>
-
-                    <div class="card-body">
-                        <ul>
-                            @foreach ($posts as $post)
-                                <li>{{$post->title}}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="container">
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Title</th>
+                <th scope="col">Slug</th>
+                <th scope="col">Actions</th>
+                {{-- Altrimenti i bordi fanno schifo --}}
+                <th scope="col"></th>
+                <th scope="col"></th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($posts as $post)
+                <tr>
+                    <th scope="row">{{$post->id}}</th>
+                    <td>{{$post->title}}</td>
+                    <td>{{$post->slug}}</td>
+                    <td>
+                        <a href="{{route("posts.show", $post->id)}}"><button type="button" class="btn btn-primary">Più info</button></a>
+                    </td>
+                    <td>
+                        <a href="{{"posts.update", $post->id}}"><button type="button" class="btn btn-warning">Modifica</button></a>
+                    </td>
+                    <td>
+                        <a href="{{"posts.destroy", $post->id}}"><button type="button" class="btn btn-danger">Elimina</button></a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 @endsection
