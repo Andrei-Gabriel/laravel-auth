33cbf53e3f1239b9a43d9bb4c2a69b1f9f149174
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2022-02-16 17:22:57 +0100

    php artisan db:seed --class=PostsTableSeedereeder

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+// Per usare la funzione slug()
+use Illuminate\Support\Str;
+use App\Post;
 
 class PostsTableSeeder extends Seeder
 {
@@ -9,8 +13,15 @@ class PostsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i = 0; $i < 10; $i++){
+            $newPost = new Post();
+            $newPost->title = $faker->words(7, true);
+            $newPost->slug = Str::of($newPost->title)->slug("-"); // Separatore a nostra scelta
+            $newPost->content = $faker->text(600);
+            $newPost->published = rand(0,1);
+            $newPost->save();
+        }
     }
 }
