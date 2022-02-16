a7c76959f5c1f21fa999c0409c3a05efa1ddfa6e
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2022-02-16 22:01:32 +0100

    Migliorato show

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -5,9 +5,16 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">{{$post->title}}</div>
+                    <h3 class="card-header">{{$post->title}}</h3>
 
                     <div class="card-body">
+                        <div class="mb-2">
+                            @if ($post->published)
+                                <span class="badge badge-success py-1 px-2">Published</span>
+                            @else
+                                <span class="badge badge-warning py-1 px-2">Draft</span>
+                            @endif
+                        </div>
                         {{$post->content}}
                     </div>
                 </div>
